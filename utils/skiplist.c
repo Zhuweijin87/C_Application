72f91140e3f74b5114774f990e42d0d4da5d4f6b
Zhuweijin87 <zhuweijin87@github.com>
2017-07-16 07:57:45 -0700

    add skiplist

---
+++ utils/skiplist.c
@@ -0,0 +1,16 @@
+/********************************
+ *   跳表
+ *   ****************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define  SKIP_LIST_MAX		8
+
+typedef struct skip_node_s
+{
+	
+} skip_node_t;
