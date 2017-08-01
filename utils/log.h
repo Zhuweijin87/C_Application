22d36f600f29b823e208a70a0a873b91cc0aa06d
Weijin Zhu <472214158@qq.com>
2017-08-01 10:02:56 +0800

    Create log.h

---
+++ utils/log.h
@@ -0,0 +1,27 @@
+#ifndef _LOG_H
+#define _LOG_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define ll_debug        4
+#define ll_warn         3
+#define ll_info         2
+#define ll_error        1
+
+#define MAX_LINE_SIZE           2048
+
+void loginit(int level, char *path, char *outfile);
+
+void rawlog(int level, const char *file, int line, char *fmt, ...);
+
+#define V_INFO(args)  __FILE__,__LINE__
+#define elog(level, ...) rawlog(level, V_INFO(args), __VA_ARGS__)
+
+#endif
